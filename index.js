5a5493a47d9a5d55aa8f3e2f5fe46ca87c835f16
zhangchao <529743595@qq.com>
2021-08-19 10:22:06 +0800

    feat: add arg oss_tag

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 #!/usr/bin/env node
-// 读取当前环境
-var modeIndex = process.argv.findIndex(function (v) { return v === '--mode'; });
-var currentMode = modeIndex >= 0 ? process.argv[modeIndex + 1] : undefined;
-require("dotenv").config(currentMode && { path: require('path').resolve(process.cwd(), ".env." + currentMode) });
-console.log('当前环境为:', currentMode);
-if (!process.env.OSS_TAG) {
+var argModeIndex = process.argv.findIndex(function (v) { return v === '--mode'; });
+var argTagIndex = process.argv.findIndex(function (v) { return v === '--oss_tag'; });
+var argMode = argModeIndex >= 0 ? process.argv[argModeIndex + 1] : undefined;
+var argTag = argTagIndex >= 0 ? process.argv[argTagIndex + 1] : undefined;
+require("dotenv").config({ path: require('path').resolve(process.cwd(), ".env" + (argMode ? "." + argMode : '')) });
+console.log('当前环境为:', argTag || '默认（.env）');
+var currentTag = argTag || process.env.OSS_TAG;
+if (!currentTag) {
     console.log('未设置 OSS_TAG 环境变量');
     process.exit(0);
 }
 var child_process = require('child_process');
 var spawn = child_process.spawn;
 var getRemoteTags = function () { return spawn('git', ['fetch', '--tags']); };
-var getLatestTags = function () { return spawn('git', ['tag', '-l', '--sort=-v:refname', process.env.OSS_TAG + "*"]); };
+var getLatestTags = function () { return spawn('git', ['tag', '-l', '--sort=-v:refname', currentTag + "*"]); };
 var getAddTag = function (version) { return spawn('git', ['tag', '-a', version, '-m', 'deploy test version']); };
 var getPushTag = function () { return spawn('git', ['push', '--tags']); };
 getRemoteTags().stdout.on('end', function () {
     console.log('拉取成功~');
     getLatestTags().stdout.on('data', function (chunk) {
         console.log('读取成功~');
-        var latestTag = chunk.toString().split('\n')[0] || process.env.OSS_TAG + "-v0.0.0";
+        var latestTag = chunk.toString().split('\n')[0] || currentTag + "-v0.0.0";
         console.log('当前最新值为:', latestTag);
         var array = latestTag.split('.');
         array.splice(-1, 1, (Number(array.slice(-1)) + 1).toString());
--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
-// 读取当前环境
-const modeIndex = process.argv.findIndex(v => v === '--mode');
-const currentMode = modeIndex >= 0 ? process.argv[modeIndex + 1] : undefined;
-require("dotenv").config({path: require('path').resolve(process.cwd(), `.env${currentMode ? `.${currentMode}` : ''}`)});
-console.log('当前环境为:', currentMode || '默认（.env）');
-if (!process.env.OSS_TAG) {
+const argModeIndex = process.argv.findIndex(v => v === '--mode');
+const argTagIndex = process.argv.findIndex(v => v === '--oss_tag');
+const argMode = argModeIndex >= 0 ? process.argv[argModeIndex + 1] : undefined;
+const argTag = argTagIndex >= 0 ? process.argv[argTagIndex + 1] : undefined;
+require("dotenv").config({path: require('path').resolve(process.cwd(), `.env${argMode ? `.${argMode}` : ''}`)});
+console.log('当前环境为:', argTag || '默认（.env）');
+
+const currentTag = argTag || process.env.OSS_TAG
+
+if (!currentTag) {
   console.log('未设置 OSS_TAG 环境变量');
   process.exit(0);
 }
@@ -13,7 +17,7 @@ const child_process = require('child_process');
 const {spawn} = child_process;
 
 const getRemoteTags = () => spawn('git', ['fetch', '--tags']);
-const getLatestTags = () => spawn('git', ['tag', '-l', '--sort=-v:refname', `${process.env.OSS_TAG}*`]);
+const getLatestTags = () => spawn('git', ['tag', '-l', '--sort=-v:refname', `${currentTag}*`]);
 const getAddTag = (version) => spawn('git', ['tag', '-a', version, '-m', 'deploy test version']);
 const getPushTag = () => spawn('git', ['push', '--tags']);
 
@@ -21,7 +25,7 @@ getRemoteTags().stdout.on('end', () => {
   console.log('拉取成功~');
   getLatestTags().stdout.on('data', chunk => {
     console.log('读取成功~');
-    const latestTag = chunk.toString().split('\n')[0] || `${process.env.OSS_TAG}-v0.0.0`;
+    const latestTag = chunk.toString().split('\n')[0] || `${currentTag}-v0.0.0`;
     console.log('当前最新值为:', latestTag);
     const array = latestTag.split('.');
     array.splice(-1, 1, (Number(array.slice(-1)) + 1).toString());
